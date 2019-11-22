70afdb187dbcff93973cd76326cc38299c198337
chnirt <trinhchinchin@gmail.com>
2019-11-22 10:29:46 +0000

    Fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,10 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <div style={{ color: `purple` }}>
+      <Link to="/about/">Contact</Link>
+      <p>What a world.</p>
+    </div>
   </Layout>
 )
 
